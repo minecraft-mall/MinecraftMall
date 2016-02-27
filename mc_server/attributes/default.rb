d6436e7b7ba93d5f04536e62803884dbcc5e12c1
unknown <>
2016-02-27 19:08:27 +0900

    add firewall

--- mc_server/attributes/default.rb
+++ mc_server/attributes/default.rb
@@ -16,6 +16,9 @@ default['mc_server']['jre']['file'] = 'jre-8u73-windows-x64.exe'
 default['mc_server']['jre']['dir'] = node['mc_server']['root']['wk_dir']
 default['mc_server']['jre']['INSTALLDIR'] = 'C:\Java'
 
+default['mc_server']['firewall']['name'] = 'minecraft_server'
+default['mc_server']['firewall']['port'] =  25565
+
 default['mc_server']['download']['files']['installer'] = 'http://files.minecraftforge.net/maven/net/minecraftforge/forge/1.8.9-11.15.1.1722/forge-1.8.9-11.15.1.1722-installer.jar'
 default['mc_server']['download']['files']['universal'] = 'http://files.minecraftforge.net/maven/net/minecraftforge/forge/1.8.9-11.15.1.1722/forge-1.8.9-11.15.1.1722-universal.jar'
 default['mc_server']['download']['dir'] = node['mc_server']['root']['dir']
