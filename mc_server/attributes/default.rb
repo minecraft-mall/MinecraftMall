a04084cbf96684bfc74e0854e029a55531434051
unknown <>
2016-02-27 19:34:07 +0900

    add download

--- mc_server/attributes/default.rb
+++ mc_server/attributes/default.rb
@@ -19,8 +19,8 @@ default['mc_server']['jre']['INSTALLDIR'] = 'C:\Java'
 default['mc_server']['firewall']['name'] = 'minecraft_server'
 default['mc_server']['firewall']['port'] =  25565
 
-default['mc_server']['download']['files']['installer'] = 'http://files.minecraftforge.net/maven/net/minecraftforge/forge/1.8.9-11.15.1.1722/forge-1.8.9-11.15.1.1722-installer.jar'
-default['mc_server']['download']['files']['universal'] = 'http://files.minecraftforge.net/maven/net/minecraftforge/forge/1.8.9-11.15.1.1722/forge-1.8.9-11.15.1.1722-universal.jar'
+default['mc_server']['download']['files']['installer.jar'] = 'http://files.minecraftforge.net/maven/net/minecraftforge/forge/1.8.9-11.15.1.1722/forge-1.8.9-11.15.1.1722-installer.jar'
+default['mc_server']['download']['files']['universal.jar'] = 'http://files.minecraftforge.net/maven/net/minecraftforge/forge/1.8.9-11.15.1.1722/forge-1.8.9-11.15.1.1722-universal.jar'
 default['mc_server']['download']['dir'] = node['mc_server']['root']['dir']
 
 default['mc_server']['install']['dir'] = node['mc_server']['root']['dir']
